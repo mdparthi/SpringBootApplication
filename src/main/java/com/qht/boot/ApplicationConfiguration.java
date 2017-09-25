4a0717211b1f6da929d8e590a22d386b1401b1cb
Parthiban Rey <maddyparthi11@gmail.com>
2017-09-25 15:44:58 +0530

    updated git ignore

--- src/main/java/com/qht/boot/ApplicationConfiguration.java
+++ src/main/java/com/qht/boot/ApplicationConfiguration.java
@@ -2,11 +2,21 @@ package com.qht.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //opinated approach to load the necessary beans
 @EnableAutoConfiguration
+@Controller
 public class ApplicationConfiguration {
 
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "hello world";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationConfiguration.class, args);
 	}
